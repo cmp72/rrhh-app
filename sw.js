6ca35d8df2306d1a3b15ba50c12a505703166105
cmp72 <102181904+cmp72@users.noreply.github.com>
2026-05-30 12:39:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,21 @@
-const CACHE = 'labora-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const VERSION = 'v1';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+      Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    fetch(e.request, { cache: 'no-store' }).catch(() =>
+      caches.match(e.request)
+    )
   );
 });
